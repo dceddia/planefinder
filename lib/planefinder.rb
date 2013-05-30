9277ec72a182a11e2479e02351ce5e09c8b8647a
Dave Ceddia <dceddia@gmail.com>
2013-05-29 22:38:48 -0400

    added test for getting categories

--- lib/planefinder.rb
+++ lib/planefinder.rb
@@ -1,5 +1,16 @@
 require "planefinder/version"
+require "httparty"
+require "json"
 
 module Planefinder
-  # Your code goes here...
+  include HTTParty
+  base_uri 'www.trade-a-plane.com'
+
+  @@urls = {}
+  @@urls[:airplane_categories] = "/app_ajax/get_categories?listing_type_id=1"
+
+  def self.get_categories
+    response = self.get(@@urls[:airplane_categories])
+    JSON.parse(response.body)
+  end
 end
--- spec/planefinder_spec.rb
+++ spec/planefinder_spec.rb
@@ -1,5 +1,22 @@
+require 'planefinder'
+require 'fakeweb'
+
 describe Planefinder do
   it "should have a version" do
     Planefinder::VERSION.should_not be_empty
+  end
+
+  context "retrieving categories" do
+    before do
+      FakeWeb.allow_net_connect = false
+      response_json = File.join(File.dirname(__FILE__), 'fixtures', 'airplane_categories.json')
+      FakeWeb.register_uri(:get, 'http://www.trade-a-plane.com/app_ajax/get_categories?listing_type_id=1', :body => response_json)
+    end
+
+    it "should retrieve aircraft categories" do
+      categories = Planefinder.get_categories
+      categories.length.should == 10
+      categories.each { |c| c.class.should == AirplaneCategory.class }
+    end
   end
 end
