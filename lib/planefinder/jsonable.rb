eb8d53641cf69b0920d4b4ec9c6d670e277e9c4d
Dave Ceddia <dceddia@gmail.com>
2013-09-22 13:57:24 -0400

    updated jsonable to leave off leading @ signs from instance vars

--- lib/planefinder/jsonable.rb
+++ lib/planefinder/jsonable.rb
@@ -1,16 +1,11 @@
 # Handily stolen from Stack Overflow (david4dev): http://stackoverflow.com/questions/4464050/ruby-objects-and-json-serialization-without-rails
 module JSONable
-  def to_json(arg)
+  def to_json(arg=nil)
     puts arg
     hash = {}
     self.instance_variables.each do |var|
-      hash[var] = self.instance_variable_get var
+      hash[var.to_s.gsub(/^@/, '')] = self.instance_variable_get var
     end
     hash.to_json
-  end
-  def from_json! string
-    JSON.load(string).each do |var, val|
-      self.instance_variable_set var, val
-    end
   end
 end
